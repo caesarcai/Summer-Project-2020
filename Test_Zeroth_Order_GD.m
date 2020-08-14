2d89f9af2bc04056bdc588fc00c6e12951589daa
AlphaGoLYC <56068255+AlphaGoLYC@users.noreply.github.com>
2020-08-15 07:44:47 +0800

    Add files via upload

---
+++ Test_Zeroth_Order_GD.m
@@ -0,0 +1,71 @@
+% ================= Check Zeroth Order Gradient Descent ============== %
+% Script that tests a simple version of ZORO, and plots results
+% with FISTA and with CoSamp.
+% Daniel Mckenzie and HanQin Cai
+% 2nd July 2019 modified March/April 2020
+% August 2020 modified by Yuchen Lou
+% 
+% ==================================================================== %
+
+% Test on Quadratic Program
+
+clear, clc, close all
+
+
+% ======================== Function and Oracle Parameters ============ %
+D = 1200; % ambient dimension
+s = 120; % function sparsity
+J = 10; % number of blocks
+noise_level = 0.01; % noise level
+S = datasample(1:D,s,'Replace',false);  % randomly choose the support of ...
+% the sparse quadric.
+% ================================ ZORO Parameters ==================== %
+%num_samples = ceil(2*s*log(D));
+num_samples = J*ceil(s*log(s)^2*log(D)^2/120/J);
+num_iterations = 100; % number of iterations
+delta1 = 0.0005;
+step_size = 0.1;% Step size for BCD ZORO
+x0 = randn(D,1);
+
+% ========================= Some additional parameters ================= %
+[~,true_grad] = SparseQuadric(x0,S,D,noise_level);
+init_grad_estimate = norm(true_grad);
+true_min = 0;
+
+% ==== Run with CoSamp, high tolerance
+tol = 5e-8;%5e-2;
+[f_hat_COSAMP,x_hat_COSAMP,regret_COSAMP,time_vec_COSAMP,gradient_norm] = ZerothOrderGD_CoSamp(num_iterations,step_size,x0,true_min,S,D,noise_level,num_samples, delta1,init_grad_estimate,tol,J,"Full");
+[f_hat_COSAMP2,x_hat_COSAMP2,regret_COSAMP2,time_vec_COSAMP2,gradient_norm2] = ZerothOrderGD_CoSamp(num_iterations,step_size,x0,true_min,S,D,noise_level,num_samples, delta1,init_grad_estimate,tol,J,"FullBD");
+[f_hat_COSAMP3,x_hat_COSAMP3,regret_COSAMP3,time_vec_COSAMP3,gradient_norm3] = ZerothOrderGD_CoSamp(num_iterations,step_size,x0,true_min,S,D,noise_level,num_samples, delta1,init_grad_estimate,tol,J,"FullCirculant");
+[f_hat_COSAMP4,x_hat_COSAMP4,regret_COSAMP4,time_vec_COSAMP4,gradient_norm4] = ZerothOrderGD_CoSamp(num_iterations,step_size,x0,true_min,S,D,noise_level,num_samples, delta1,init_grad_estimate,tol,J,"BCD");
+[f_hat_COSAMP5,x_hat_COSAMP5,regret_COSAMP5,time_vec_COSAMP5,gradient_norm5] = ZerothOrderGD_CoSamp(num_iterations,step_size,x0,true_min,S,D,noise_level,num_samples, delta1,init_grad_estimate,tol,J,"BCCD");
+[f_hat_COSAMP6,x_hat_COSAMP6,regret_COSAMP6,time_vec_COSAMP6,gradient_norm6] = ZerothOrderGD_CoSamp(num_iterations,step_size,x0,true_min,S,D,noise_level,num_samples, delta1,init_grad_estimate,tol,J,"FullBC");
+
+% === Plot results
+figure
+hold on
+plot(num_samples*(1:num_iterations),regret_COSAMP,'r*')
+plot(num_samples*(1:num_iterations),regret_COSAMP2,'g^')
+plot(num_samples*(1:num_iterations),regret_COSAMP3,'b^')
+plot(num_samples*(1:num_iterations),regret_COSAMP6,'k^')
+plot(num_samples*(1:num_iterations),regret_COSAMP4,'rs')
+plot(num_samples*(1:num_iterations),regret_COSAMP5,'ks')
+set(gca,'Yscale','log')
+title('Optimization Error','FontSize',16)
+legend('Full random','Full BD','Full Circ','Full BC','BCD','BCCD','FontSize',14)
+xlabel('Queries','FontSize',14)
+
+
+figure
+hold on
+plot(num_samples*(1:num_iterations),time_vec_COSAMP,'r*')
+plot(num_samples*(1:num_iterations),time_vec_COSAMP2,'r^')
+plot(num_samples*(1:num_iterations),time_vec_COSAMP3,'b^')
+plot(num_samples*(1:num_iterations),time_vec_COSAMP3,'k^')
+plot(num_samples*(1:num_iterations),time_vec_COSAMP4,'rs')
+plot(num_samples*(1:num_iterations),time_vec_COSAMP5,'bs')
+title('Cumulative Run Time','FontSize',16)
+legend('Full random','Full BD','Full Circ','Full BC','BCD','BCCD','FontSize',14)
+ylabel('Time(s)','FontSize',14)
+xlabel('Queries','FontSize',14)
+
---
+++ ZerothOrderGD_CoSamp.m
@@ -0,0 +1,134 @@
+function [f_hat,x_hat,regret,time_vec,gradient_norm] = ZerothOrderGD_CoSamp(num_iterations,step_size,x0,true_min,S,D,noise_level,num_samples, delta1,grad_estimate,tol,J,Type)
+%        [f_hat,x_hat] = ZerothOrderGD_CoSamp(num_iterations,step_size)
+% This function runs a simple Zeroth-order gradient descent for the
+% SparseQuadric function.
+% Uses CoSamp to solve the sparse recovery problem.
+% Daniel Mckenzie
+% 26th June 2019
+% Modified by Yuchen Lou in August 2020
+%
+
+
+x = x0;
+regret = zeros(num_iterations,1);
+time_vec = zeros(num_iterations,1);
+gradient_norm = zeros(num_iterations,1);
+%sparsity = ceil(1.5*length(S));
+sparsity = length(S);
+
+if (Type == "Full")
+    % Usual Sensing
+    Z = 2*(rand(num_samples,D) > 0.5) - 1;
+    
+elseif (Type == "FullBD")
+    % Full Random Block Diagonal Sensing
+    Z = zeros(num_samples,D);
+    m = num_samples/J; n = D/J;
+    Z1 = 2*(rand(m,n) > 0.5) - 1;
+    for i = 0:(J-1)
+        Z((m*i+1):(m*i+m),(n*i+1):(n*i+n)) = Z1;
+    end
+elseif (Type == "FullCirculant")
+    % Circulant Sensing Matrix
+    z1 = 2*(rand(1,D) > 0.5) - 1;
+    
+    %F = dftmtx(D); % with FFT
+    %Z1 = F*diag(F*z1(:))/F;
+    
+    Z1 = gallery('circul',z1); % without FFT
+    SSet = datasample(1:D,num_samples,'Replace',false);
+    Z = Z1(SSet,:);
+    
+elseif (Type == "BCD")
+    % Block Coordinate Descent
+    m = num_samples/J; n = D/J;
+    Z = 2*(rand(m,n) > 0.5) - 1;
+    
+elseif (Type == "BCCD")
+    % Block Circulant Coordinate Descent
+    m = num_samples/J; n = D/J;
+    z1 = 2*(rand(1,n) > 0.5) -1;
+    Z1 = gallery('circul',z1);
+    SSet = datasample(1:n,m,'Replace',false);
+    Z = Z1(SSet,:);
+    
+elseif (Type == "FullBC")
+    % Full GD with block circulant sensing matrix
+    m = num_samples/J; n = D/J;
+    z1 = 2*(rand(1,n) > 0.5) -1;
+    Z1 = gallery('circul',z1);
+    SSet = datasample(1:n,m,'Replace',false);
+    Z2 = Z1(SSet,:);
+    Z = zeros(num_samples,D);
+    for i = 0:(J-1)
+        Z((m*i+1):(m*i+m),(n*i+1):(n*i+n)) = Z2;
+    end
+end
+
+if (Type == "BCD") || (Type == "BCCD")
+    
+    for i = 1:num_iterations
+        tic
+        %i
+        delta = delta1 * norm(grad_estimate);
+        
+        coord_index = randi(J);% randomly select a block
+        S_block = [];
+        for j = 1:length(S)
+            if S(j)>=(coord_index-1)*n+1 && S(j)<=coord_index*n
+                S_block = [S_block S(j)-n*(coord_index-1)];
+            end
+        end % Find significant index in the selected block
+        x_block = x((coord_index-1)*n+1:coord_index*n);
+        sparsity = length(S_block);
+        
+        [~,grad_estimate_block] = CosampGradEstimate(x_block,m,delta,S_block,n,noise_level,tol,sparsity,Z);
+        grad_estimate = zeros(length(x),1);
+        grad_estimate((coord_index-1)*n+1:coord_index*n) = grad_estimate_block;
+        x = x - step_size*grad_estimate;
+        [f_est,~] = SparseQuadric(x,S,D,noise_level);
+        %gradient_norm(i) = nnz(grad_estimate);
+        %sparsity = gradient_norm(i);
+        %regret(i) = abs((f_est - true_min)/true_min);  % relative error
+        regret(i) = abs((f_est - true_min));
+        %grad_estimate(S)
+        if i==1
+            time_vec(i) = toc;
+        else
+            time_vec(i) = time_vec(i-1) + toc;
+        end
+        if sparsity == 0
+            break
+        end
+    end
+    
+else
+    for i = 1:num_iterations
+        tic
+        %i
+        delta = delta1 * norm(grad_estimate);
+        [~,grad_estimate] = CosampGradEstimate(x,num_samples,delta,S,D,noise_level,tol,sparsity,Z);
+        x = x - step_size*grad_estimate;
+        [f_est,~] = SparseQuadric(x,S,D,noise_level);
+        %gradient_norm(i) = nnz(grad_estimate);
+        %sparsity = gradient_norm(i);
+        %regret(i) = abs((f_est - true_min)/true_min);  % relative error
+        regret(i) = abs((f_est - true_min));
+        %grad_estimate(S)
+        if i==1
+            time_vec(i) = toc;
+        else
+            time_vec(i) = time_vec(i-1) + toc;
+        end
+        if sparsity == 0
+            break
+        end
+    end
+    
+end
+
+x_hat = x;
+[f_hat,~] = SparseQuadric(x_hat,S,D,0);
+
+end
+
